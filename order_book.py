238a077251ff9f407ee3f88a68813e5f523010c9
thomascox <coxt@seas.upenn.edu>
2022-11-14 17:16:49 -0500

    commit

--- order_book.py
+++ order_book.py
@@ -32,9 +32,10 @@ def process_order(order, match=None):
         tstamp = datetime.now()
         _order.filled = tstamp
         match.filled = tstamp
-        _order.counterpart_id = ForeignKey('match.id')
-        match.counterpart_id = ForeignKey('_order.id')
-
+        _order.counterpart_id = match.id
+        _order.counterparty.append(match.id)
+        match.counterpart_id = _order.id
+        match.counterparty.append(_order.id)
         if match.buy_amount > _order.sell_amount:
             child_buy = match.buy_amount - _order.sell_amount
             child_sell = (match.sell_amount/match.buy_amount)*(match.buy_amount-_order.sell_amount)

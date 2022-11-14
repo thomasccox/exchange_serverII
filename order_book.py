5a576f6837198df623d7df5511812a2e30ca8806
thomascox <coxt@seas.upenn.edu>
2022-11-14 13:47:25 -0500

    commit

--- order_book.py
+++ order_book.py
@@ -33,7 +33,7 @@ def process_order(order):
 def find_match(order):
     sell_currency = order['sell_currency']
     buy_currency = order['buy_currency']
-    potential_matches = session.query(Order).filter(Order.filled =="None").all()
+    potential_matches = session.query(Order).filter(Order["filled"] == "None").all()
     #potential_matches = session.query(Order).all()
     #print("test")
     for o in potential_matches:
